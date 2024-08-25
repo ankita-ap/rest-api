a90b94bee67749443b5b0e7f67d24a2aa6127879
ankita-ap <143788895+ankita-ap@users.noreply.github.com>
2024-08-25 11:38:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
 
-const findHighestLowercase = (arr) => {
-    const lowercaseAlphabets = arr.filter(char => char >= 'a' && char <= 'z');
-    return lowercaseAlphabets.sort().reverse()[0] || null;
-};
+app.post('/bfhl', (req, res) => {
+    const { first_name, last_name, dob } = req.body;
 
-
-app.get('/endpoint', (req, res) => {
-    const operationCode = 'OPERATION_85432'; 
-    res.status(200).send({
-        operation_code: operationCode
-    });
-});
-
-
-app.post('/endpoint', (req, res) => {
-    const { user_id, college_email_id, college_roll_number, input_array } = req.body;
-
-    if (!user_id || !college_email_id || !college_roll_number || !input_array) {
-        return res.status(400).send({ 
-            message: 'Please provide user_id, college_email_id, college_roll_number, and input_array in the request body.'
+    
+    if (!first_name || !last_name || !dob) {
+        return res.status(400).send({
+            is_success: false,
+            message: 'Please provide first_name, last_name, and dob in the request body.'
         });
     }
 
-    const numbersArray = input_array.filter(item => typeof item === 'number');
-    const alphabetsArray = input_array.filter(item => typeof item === 'string' && /^[a-zA-Z]$/.test(item));
-    const highestLowercaseAlphabet = findHighestLowercase(alphabetsArray);
+    
+    const formatted_dob = dob.split('-').reverse().join(''); 
+    const user_id = `${first_name.toLowerCase()}_${last_name.toLowerCase()}_${formatted_dob}`;
 
+    
     res.status(200).send({
-        status: 'Success',
-        user_id: user_id,
-        college_email_id: college_email_id,
-        college_roll_number: college_roll_number,
-        numbers: numbersArray,
-        alphabets: alphabetsArray,
-        highest_lowercase: highestLowercaseAlphabet
+        is_success: true,
+        user_id: user_id
     });
 });
 
-app.listen(
-    PORT,
-    () => console.log(`It's alive on http://localhost:${PORT}`)
-);
+
+app.get('/bfhl', (req, res) => {
+    res.status(200).send({
+        operation_code: 1
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`It's alive on http://localhost:${PORT}`);
+});
